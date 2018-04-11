1b8ae41f760ac254f47ceceeae3653618e507cad
User <user@Users-MacBook-Pro-2.local>
2018-04-11 11:22:44 +0300

    Built iPhone interface

--- dev-profile/dev-profile/ViewController.swift
+++ dev-profile/dev-profile/ViewController.swift
@@ -2,8 +2,8 @@
 //  ViewController.swift
 //  dev-profile
 //
-//  Created by User on 10.04.2018.
-//  Copyright © 2018 MPTechnologies. All rights reserved.
+//  Created by Mark Price on 6/16/17.
+//  Copyright © 2017 Devslopes. All rights reserved.
 //
 
 import UIKit
